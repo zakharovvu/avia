592abaee14557a6f56486c0d3933129bc41da0bd
Slava <zakharovvu@gmail.com>
2019-05-04 18:43:44 +0300

    Updates

---
+++ precache-manifest.d7092a259313b1194bebf5293203f4eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a7075cdf1b38e05597be2389c5d726",
+    "url": "/avia/index.html"
+  },
+  {
+    "revision": "2f13fe0c95f5bc0b41e0",
+    "url": "/avia/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "4c5146ca5b0f22b49422",
+    "url": "/avia/static/css/main.89954a38.chunk.css"
+  },
+  {
+    "revision": "2f13fe0c95f5bc0b41e0",
+    "url": "/avia/static/js/2.30ffa0c1.chunk.js"
+  },
+  {
+    "revision": "4c5146ca5b0f22b49422",
+    "url": "/avia/static/js/main.d80a15f2.chunk.js"
+  },
+  {
+    "revision": "f85b888eb6d2e33a229c",
+    "url": "/avia/static/js/runtime~main.c9c8095b.js"
+  },
+  {
+    "revision": "4629a572f4ffa3898beacfec7adfc909",
+    "url": "/avia/static/media/logo.4629a572.png"
+  }
+]);
